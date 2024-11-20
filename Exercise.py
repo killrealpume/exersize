72bff1145220f3d04be1d81fad93295eb0b243a4
killrealpume <163526482+killrealpume@users.noreply.github.com>
2024-11-20 17:17:27 +0200

    Add files via upload

---
+++ Exercise.py
@@ -0,0 +1,12 @@
+#"1st program"
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 !=1000.1)
+#"3rd program"
+print(2 * 2+2 == 2 * (2 + 2))
+#"4th program"
+print(int(float('123.456') * 10) % 10)
+#из доп задач
+#"3rd program"
+print(int(float("1234") / 10) % 100 + int(float("5678") / 10) % 100)
+
